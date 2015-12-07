20ce7c8688662ea3e22e1071f52d9f9e8cc904d6
Chris Grandin <cgrandin@shaw.ca>
2015-12-07 14:16:15 -0800

    Made latex compilation messages go to latexOutput.log instead of console.

--- doc/buildtex.bat
+++ doc/buildtex.bat
@@ -1,3 +1,3 @@
 Rscript -e "library(knitr);knit('./example.Rnw')" 1> knitrOutput.log 2>&1
 
-@latex -synctex=1 "example.tex" && bibtex "example" && latex "example.tex" && latex "example.tex" && dvips "example.dvi" && ps2pdf "example.ps"
+(@latex -synctex=1 "example.tex" && bibtex "example" && latex "example.tex" && latex "example.tex" && dvips "example.dvi" && ps2pdf "example.ps") 1> latexOutput.log 2>&1
